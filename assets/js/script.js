d03e0c46291cdfcc41f82598dc216a1eb0c8b1c1
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-01-29 15:22:59 -0600

    set date added

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/*
+1) hard code the html blocks for the time to start
+    each hour block is its own div
+    within it are the text of the current hour, a text block and then the save button
+2) use moment to pull in current day/time DONE
+3)
+
+
+*/
+var setDate = function () {
+    // create date variable with date formated to spec
+    var date = moment().format("dddd, MMMM, Do");
+    console.log(date); // log to test
+    $('#currentDay').text(date); // set text value of currentDay to the date
+};
+
+var saveSchedule = function () {
+    // save the array of timeblocks to local storage stirngy etc
+
+};
+
+var loadSchedule = function() {
+
+        // code to create an array of timeblock objects
+};
+
+setDate();
